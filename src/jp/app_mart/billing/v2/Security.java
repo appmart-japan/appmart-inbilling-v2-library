4ed006bdfefa99d079b1a5871e17516f8f091b85
canu johann <canu.johann@gmail.com>
2015-06-25 18:02:13 +0900

    verifyPurchase関数を追加

--- src/jp/app_mart/billing/v2/Security.java
+++ src/jp/app_mart/billing/v2/Security.java
@@ -1,55 +1,82 @@
 package jp.app_mart.billing.v2;
 
+import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 import javax.crypto.Cipher;
 
-import android.util.Base64;
 import android.util.Log;
 
+/**
+ * 
+ * @author canujohann
+ * appmartアプリ内課金のsignatureをチェックするクラス
+ */
 public class Security {
 
-	private static final String TAG = "IABUtil/Security";
+	private static final String TAG = "billing/v2/Security";
+		
+	/*
+	* signatureをチェック(verify)
+	* @param signature : signature
+	* @param origina: original 元の文字列
+	* @param publicKey: 公開鍵 
+	*/
+	public static boolean verifyPurchase(String publicKey,String signature, String original){
+
+		try{
 	
-    public static boolean verifyPurchase(String strPublicKey, String stringEncrypted, String orderId) {
-    	if (orderId == null) {
-            Log.e(TAG, "data is null");
-            return false;
-        }    
-        if(!encryptRSAToString(orderId,strPublicKey).equals(stringEncrypted)){
-            Log.w(TAG, "signature does not match data.");
-            return false;
-        }
-        return true;
-    }
-    
-    /*
-     * Encrypt data with the public key
-     */
-	public static String encryptRSAToString(String text, String strPublicKey) {
-	    byte[] cipherText = null;
-	    String strEncryInfoData="";
-	    try {
-	    	
-	    	KeyFactory keyFac = KeyFactory.getInstance("RSA");
-			KeySpec keySpec = new X509EncodedKeySpec(Base64.decode(strPublicKey.trim().getBytes(), Base64.DEFAULT));
-			Key publicKey = keyFac.generatePublic(keySpec);
-	    	
-	    	
-			// get an RSA cipher object and print the provider
-			final Cipher cipher = Cipher.getInstance("RSA");
-			// encrypt the plain text using the public key
-			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-			cipherText = cipher.doFinal(text.getBytes());
-			strEncryInfoData = new String(Base64.encode(cipherText,Base64.DEFAULT));
-			
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }
-	    return strEncryInfoData.replaceAll("(\\r|\\n)", "");
+			// Get private key from String
+			PublicKey pk = loadPublicKey(publicKey);
+		
+			// text to bytes
+			byte[] originalBytes = original.getBytes("UTF8");
+		
+			//signature to bytes
+			byte[] signatureBytes =Base64.decode(signature);
+		
+			Signature sig = Signature.getInstance("SHA1withRSA");
+			sig.initVerify(pk);
+			sig.update(originalBytes);
+		
+			return sig.verify(signatureBytes);
+		
+		}catch(Exception e){
+			e.printStackTrace();
+			Log.e("appmartアプリ内課金","error for signature:" + e.getMessage());
+			return false;
+		}
 	}
+	
+	/*
+	* Generate a PublicKey object from a string
+	* @ key64 : public key in string format (BASE 64)
+	*/
+	private static PublicKey loadPublicKey(String key64) throws GeneralSecurityException {
+		try{
+			//byte[] data = javax.xml.bind.DatatypeConverter.parseBase64Binary(key64);
+			byte[] data = Base64.decode(key64);
+			X509EncodedKeySpec spec = new X509EncodedKeySpec(data);
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			return fact.generatePublic(spec);
+		} catch (NoSuchAlgorithmException e) {
+	        throw new RuntimeException(e);
+	    } catch (InvalidKeySpecException e) {
+	        Log.e(TAG, "Invalid key specification.");
+	        throw new IllegalArgumentException(e);
+	    } catch (Base64DecoderException e) {
+	        Log.e(TAG, "Base64 decoding failed.");
+	        throw new IllegalArgumentException(e);
+	    }
+	}
+	
+	
     
 }
